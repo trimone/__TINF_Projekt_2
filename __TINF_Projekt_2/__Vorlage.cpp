c3fc3ed243680248893bbb84985443d63dfc5076
Alex <alex.huber247@gmail.com>
2018-12-17 23:29:16 +0100

    LOG(X) Makro für 'cout <<'-Ausgabe

--- __TINF_Projekt_2/__Vorlage.cpp
+++ __TINF_Projekt_2/__Vorlage.cpp
@@ -28,6 +28,7 @@
 //
 //// === MAKROS ===
 //#define xHoch2(x) (x)*(x) // BESSERES Makros !!!
+//#define COUT(x) cout << x << endl	// 'cout' verkürzen => LOG (X);
 //#define ANZ 45 
 //
 //// === PROTOTYPEN ===
